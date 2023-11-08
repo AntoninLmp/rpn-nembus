6befa1de800636a55d8251ec91d8aecc390de93d
Rimberse <kerimpltm@gmail.com>
2023-11-08 15:06:44 +0100

    Initialize singleton

--- src/test/java/nembus/DivisionTest.java
+++ src/test/java/nembus/DivisionTest.java
@@ -1,5 +1,18 @@
 package nembus;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
 public class DivisionTest {
-    
+    private static Main rpnEvaluator;
+
+    @BeforeAll
+    static void init() {
+        rpnEvaluator = new Main();
+    }
+
+    @Test
+    void test() {
+
+    }
 }
