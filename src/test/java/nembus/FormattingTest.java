12e55f3685c7e1c7a0606a78fe5eb6b430c3e85b
Rimberse <kerimpltm@gmail.com>
2023-11-08 16:27:04 +0100

    Initialize singleton class

--- src/test/java/nembus/FormattingTest.java
+++ src/test/java/nembus/FormattingTest.java
@@ -1,5 +1,14 @@
 package nembus;
 
+import org.junit.jupiter.api.BeforeAll;
+
 public class FormattingTest {
+    private static Main rpnEvaluator;
+
+    @BeforeAll
+    static void init() {
+        rpnEvaluator = new Main();
+    }
+
     
 }
