1f6705663ba8bc530d989775c70c0e86b044ce95
LonXag <73125125+LonXag@users.noreply.github.com>
2023-11-08 16:48:07 +0100

    Linting additionTest

--- src/test/java/nembus/AdditionTest.java
+++ src/test/java/nembus/AdditionTest.java
@@ -7,20 +7,24 @@ import org.junit.jupiter.api.Test;
 
 public class AdditionTest {
     private static Main main;
+
     @BeforeAll
     static void setup() {
         main = new Main();
     }
+
     @Test
     void additionTestShouldReturn10() {
         int result = main.evalRPN("6 4 +");
         Assertions.assertEquals(10, result);
     }
+
     @Test
     void additionTestShouldReturn28() {
         int result = main.evalRPN("20 8 +");
         Assertions.assertEquals(28, result);
     }
+    
     @Test
     void multiplesAdditionTestShouldReturn30() {
         int result = main.evalRPN("12 8 + 10 +");
